fd49b525a58bdd4eb26e31631058209844b6c833
Ikemous <57930755+ikemous@users.noreply.github.com>
2022-12-28 09:52:14 -0700

    removed herokuapps

--- client/src/utils/projects.js
+++ client/src/utils/projects.js
@@ -1,22 +1,6 @@
 
 export default [
     {
-        name: "HALP",
-        description: "Ticket management system using a noSQL database to store login and ticket information",
-        gitlink: "https://github.com/ikemous/halp", 
-        weblink: "https://me-halp.herokuapp.com", 
-        src: "./Images/halp.PNG ",
-        languages: ["React", "Redux", "Typescript", "MongoDB", "CSS", "Node"],
-    },
-    {
-        name: "Aninder",
-        description: "Find your new furry friend using this web application. It uses multiple API calls and stores user data using a noSQL database",
-        gitlink: "https://github.com/ikemous/React-Cat-Or-Dog", 
-        weblink: "https://aninder.herokuapp.com", 
-        src: "./Images/aninder.PNG",
-        languages: ["React", "Typescript", "Redux", "Node", "CSS", "MongoDB", "API"],
-    },
-    {
         name: "Tic-Tac-Toe",
         description: "Unbeatable Tic-Tac-Toe application made using Visual Studio. Try going first or second, you won't beat this one!",
         gitlink: "https://github.com/ikemous/TicTacToe",
@@ -37,21 +21,5 @@ export default [
         weblink: "https://ikemous.github.io/weather/", 
         src: "./Images/weatherDashboard.PNG",
         languages: ["Javascript", "CSS", "HTML", "API"],
-    },
-    {
-        name: "Movie Lover",
-        description: "Web application with the purpose of searching and saving movies using the OMDB API. This application uses the MySQL Database",
-        gitlink: "https://github.com/Robbie2497/Project-2", 
-        weblink: "https://projectdash.herokuapp.com", 
-        src: "./Images/movieLover.PNG",
-        languages: ["Javascript", "API", "Node", "Handlebars", "CSS", "HTML"],
-    },
-    {
-        name: "Burger Chomper",
-        description: "Web application used with CRUD operations to track your burgers using a MySQL as the databse",
-        gitlink: "https://github.com/ikemous/burger_logger", 
-        weblink: "https://buger-eater.herokuapp.com", 
-        src: "./Images/burgerChomper.PNG",
-        languages: ["Javascript", "Node", "Handlebars", "MySQL", "CSS", "HTML"],
     },
 ];
